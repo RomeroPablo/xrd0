9b22c7e8a6d002a9640278d276161ae47853a1b3
Pablo Romero <romeropablojr03@gmail.com>
2025-09-02 12:30:43 -0500

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -6,26 +6,22 @@ import System.Exit (exitFailure)
 
 main :: IO ()
 main = do
-    -- Initialize GLFW
     success <- GLFW.init
     unless success $ do
-        putStrLn "Failed to initialize GLFW"
+        putStrLn "[!] Failed to initialize GLFW"
         exitFailure
     
-    -- Tell GLFW not to create an OpenGL context
     GLFW.windowHint $ GLFW.WindowHint'ClientAPI GLFW.ClientAPI'NoAPI
     GLFW.windowHint $ GLFW.WindowHint'Resizable False
     
-    -- Create window
-    maybeWindow <- GLFW.createWindow 800 600 "Vulkan Window" Nothing Nothing
+    maybeWindow <- GLFW.createWindow 800 600 "xrd" Nothing Nothing
     case maybeWindow of
         Nothing -> do
-            putStrLn "Failed to create GLFW window"
+            putStrLn "[!] Failed to create GLFW window"
             GLFW.terminate
             exitFailure
         Just window -> do
-            putStrLn "GLFW window created successfully!"
-            putStrLn "Press ESC or close window to exit"
+            putStrLn "[+] GLFW window created"
             
             -- Set up key callback
             GLFW.setKeyCallback window (Just keyCallback)
@@ -36,7 +32,6 @@ main = do
             -- Cleanup
             GLFW.destroyWindow window
             GLFW.terminate
-            putStrLn "Goodbye!"
 
 keyCallback :: GLFW.KeyCallback
 keyCallback window key scancode action mods
